1469d7f1a1b7e7b4ea8d088cf55fb8b42074b333
Shikha Verma <shikha.17.sv@gmail.com>
2018-02-03 01:39:27 +0530

    Profile of spy updated with status feature

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+#importing spy_details
 from spy_details import spy_name,spy_age,spy_rating
 
+#start greeting
 print ("Hello!!!")
-print('******* Welcome to SpyChat *******')
-print ("Let\'s get started")
+print('******* << Welcome to SpyChat >> *******')
+print ("Let\'s get started...\n")
 
-# Profile of a spy
+#Function declared
 def start_chat(spy_name,spy_age,spy_rating):
     show_menu = True
     while show_menu:
@@ -19,16 +21,30 @@ def start_chat(spy_name,spy_age,spy_rating):
         #More features to be added
 
 spy_exist = raw_input("Are you an existing user?(Y or N) : ")
+
+# Validating input
 if spy_exist.upper() == 'Y':
-    print("We already have your details!")
-    start_chat(spy_name, spy_age, spy_rating)    #Function called
+
+    # Existing user
+    print("We already have your details!\n")
+
+    # Function called to start chat application
+    start_chat(spy_name, spy_age, spy_rating)
 elif spy_exist.upper() == 'N':
+
+    # New user
     spy_name = raw_input("What is your spy name? ")
-    if len(spy_name)>=2: #Validation for spy name
+
+    # Checking spy has any input or not
+    if len(spy_name)>=2:
         print "Welcome " +spy_name+ ", Glad to meet you!"
         spy_salutation = raw_input("What should we call you (Mr. or Ms.)?" )
-        if len(spy_salutation)>0:      #Validation for salutation
-            spy_name = spy_salutation + " " + spy_name     #Concatenation
+
+        # Validating salutation
+        if len(spy_salutation)>0:
+
+            # Concatenation for salutation and name of spy
+            spy_name = spy_salutation + " " + spy_name
             print "Alright " + spy_name + ". I'd like to know a little bit more about you..."
             spy_age = input("Enter your age: ")
             if spy_age>=12 and spy_age<50:
@@ -42,9 +58,13 @@ elif spy_exist.upper() == 'N':
                     print("You can always do better")
                 else:
                     print("We can always use somebody to help in the office.")
-                spy_is_online = True    #Default value
+
+                # Default Value
+                spy_is_online = True
                 print "Authentication complete! Welcome %s... \nAge: %d and Rating of: %.1f\nProud to have you onboard !\n" %(spy_name,spy_age,spy_rating)
-                start_chat(spy_name,spy_age,spy_rating)        #Function called
+
+                # Function called to start chat application
+                start_chat(spy_name,spy_age,spy_rating)
             else:
                 print("Sorrry! Age inappropriate to be a spy ")
         else:
